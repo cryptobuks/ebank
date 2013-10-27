b100b6501486565b1dccaa46a3fd3ca93c27585e
Dmitry Linov <dimorph@live.ru>
2013-10-27 16:00:19 +0300

    Added new files
    Related Work Items: #64

---
+++ Proebank/InfrastructureLayer/Entities/Account.cs
@@ -0,0 +1,60 @@
+﻿using CrossCutting.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Entities
+{
+    class Account : IAccount
+    {
+        public ulong Number
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime CreationDate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEmployee Employee
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
