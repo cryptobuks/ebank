b4e36566c147af56ab7ab47fc2b8d3d4ebeddf7d
Dmitry Linov <dimorph@live.ru>
2013-11-04 02:42:16 +0300

    Added new site for internal usage by bank employees

---
+++ Proebank/PresentationLayer/Presentation.Internal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Presentation.Internal.Startup))]
+namespace Presentation.Internal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
