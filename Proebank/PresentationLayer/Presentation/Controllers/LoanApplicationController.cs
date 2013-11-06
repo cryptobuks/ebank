304f485b94ed33aadf6c97b8984cfd773c894d4e
pkartsel <pkartsel@gmail.com>
2013-11-06 03:41:38 +0300

    initial Tariffs and LoanApplications management

--- Proebank/PresentationLayer/Presentation/Controllers/LoanApplicationController.cs
+++ Proebank/PresentationLayer/Presentation/Controllers/LoanApplicationController.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Models.Accounts;
+﻿using System.Data.Entity;
+using System.Net;
+using Domain.Models.Accounts;
 using Domain.Models.Loans;
 using Microsoft.Practices.Unity;
 using System;
@@ -11,17 +13,121 @@ namespace Presentation.Controllers
 {
     public class LoanApplicationController : BaseController
     {
-        private ILoanApplicationRepository Repository { get; set; }
+        private ILoanApplicationRepository LoanApplicationRepository { get; set; }
+        private ITariffRepository TariffRepository { get; set; }
 
         public LoanApplicationController()
         {
-            Repository = Container.Resolve<ILoanApplicationRepository>();
+            LoanApplicationRepository = Container.Resolve<ILoanApplicationRepository>();
+            TariffRepository = Container.Resolve<ITariffRepository>();
         }
 
         public ActionResult Index()
         {
-            var loans = Repository.GetAll();
-            return View(loans);
+            var loanapplications = LoanApplicationRepository.GetAll();//.Include(l => l.Tariff);
+            return View(loanapplications.ToList());
         }
+
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoanApplication loanapplication = LoanApplicationRepository.Get(l => l.Id.Equals(id));
+            if (loanapplication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loanapplication);
+        }
+
+        public ActionResult Create()
+        {
+            var tariffs = TariffRepository.GetAll();
+            ViewBag.TariffId = new SelectList(tariffs, "Id", "Name");
+            return View();
+        }
+
+        // To protect from over posting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // 
+        // Example: public ActionResult Update([Bind(Include="ExampleProperty1,ExampleProperty2")] Model model)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LoanApplication loanapplication)
+        {
+            if (ModelState.IsValid)
+            {
+                loanapplication.Id = Guid.NewGuid();
+                LoanApplicationRepository.SaveOrUpdate(loanapplication);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            var tariffs = TariffRepository.GetAll();
+            ViewBag.TariffId = new SelectList(tariffs, "Id", "Name", loanapplication.TariffId);
+            return View(loanapplication);
+        }
+
+        // GET: /L/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoanApplication loanapplication = LoanApplicationRepository.Get(l => l.Id.Equals(id));
+            if (loanapplication == null)
+            {
+                return HttpNotFound();
+            }
+            var tariffs = TariffRepository.GetAll();
+            ViewBag.TariffId = new SelectList(tariffs, "Id", "Name", loanapplication.TariffId);
+            return View(loanapplication);
+        }
+
+        // To protect from over posting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // 
+        // Example: public ActionResult Update([Bind(Include="ExampleProperty1,ExampleProperty2")] Model model)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(LoanApplication loanapplication)
+        {
+            if (ModelState.IsValid)
+            {
+                LoanApplicationRepository.SaveOrUpdate(loanapplication);
+                return RedirectToAction("Index");
+            }
+            var tariffs = TariffRepository.GetAll();
+            ViewBag.TariffId = new SelectList(tariffs, "Id", "Name", loanapplication.TariffId);
+            return View(loanapplication);
+        }
+
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoanApplication loanapplication = LoanApplicationRepository.Get(l => l.Id.Equals(id));
+            if (loanapplication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loanapplication);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            LoanApplication loanapplication = LoanApplicationRepository.Get(l => l.Id.Equals(id));
+            LoanApplicationRepository.Delete(loanapplication);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
---
+++ Proebank/PresentationLayer/Presentation/Controllers/TariffsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Models.Loans;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.Practices.Unity;
+
+namespace Presentation.Controllers
+{
+    public class TariffsController : BaseController
+    {
+        private ITariffRepository TariffRepository { get; set; }
+
+        public TariffsController()
+        {
+            TariffRepository = Container.Resolve<ITariffRepository>();
+        }
+
+        public ActionResult Index()
+        {
+            var tariffs = TariffRepository.GetAll().ToList();
+            return View(tariffs);
+        }
+
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tariff tariff = TariffRepository.Get(t => t.Id.Equals(id));
+            if (tariff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tariff);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Tariffs/Create
+		// To protect from over posting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		// 
+		// Example: public ActionResult Update([Bind(Include="ExampleProperty1,ExampleProperty2")] Model model)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tariff tariff)
+        {
+            if (ModelState.IsValid)
+            {
+                tariff.Id = Guid.NewGuid();
+                TariffRepository.SaveOrUpdate(tariff);
+                return RedirectToAction("Index");
+            }
+
+            return View(tariff);
+        }
+
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tariff tariff = TariffRepository.Get(t => t.Id.Equals(id));
+            if (tariff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tariff);
+        }
+
+        // POST: /Tariffs/Edit/5
+		// To protect from over posting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		// 
+		// Example: public ActionResult Update([Bind(Include="ExampleProperty1,ExampleProperty2")] Model model)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Tariff tariff)
+        {
+            if (ModelState.IsValid)
+            {
+                TariffRepository.SaveOrUpdate(tariff);
+                return RedirectToAction("Index");
+            }
+            return View(tariff);
+        }
+
+        // GET: /Tariffs/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tariff tariff = TariffRepository.Get(t => t.Id.Equals(id));
+            if (tariff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tariff);
+        }
+
+        // POST: /Tariffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Tariff tariff = TariffRepository.Get(t => t.Id.Equals(id));
+            TariffRepository.Delete(tariff);
+            return RedirectToAction("Index");
+        }
+    }
+}
