25bcafc1d6e804df12008caaf035e7f21e733fdc
pkartsel <pkartsel@gmail.com>
2013-12-09 21:09:36 +0300

    Authorize is default requirement. Login Page don't use Layout. Main Menu is separate from Layout

--- Proebank/PresentationLayer/Presentation/Controllers/HomeController.cs
+++ Proebank/PresentationLayer/Presentation/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Presentation.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : BaseController
     {
         public ActionResult Index()
@@ -21,6 +23,13 @@ namespace Presentation.Controllers
             ViewBag.Message = "PROebank contact page.";
 
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MainMenu()
+        {
+            ViewBag.Roles = Roles.GetRolesForUser();
+            return View("~/Views/Shared/MainMenu.cshtml");
         }
     }
 }
