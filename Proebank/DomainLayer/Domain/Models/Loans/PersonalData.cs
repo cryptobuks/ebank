6f452ccf972d9efe06f96f823895876d366d7b92
pkartsel <pkartsel@gmail.com>
2013-12-17 11:36:32 +0300

    PersonalData added 2

---
+++ Proebank/DomainLayer/Domain/Models/Loans/PersonalData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+using Domain.Models.Customers;
+
+namespace Domain.Models.Loans
+{
+    /// <summary>
+    /// Customer or his guarantor document
+    /// </summary>
+    public class PersonalData : Entity
+    {
+        
+        [DisplayName("Last name")]
+        [RegularExpression("[a-zA-Z]{3,30}", ErrorMessage = "Not valid name")]
+        public string LastName { get; set; }
+
+        [DisplayName("First name")]
+        [RegularExpression("[a-zA-Z]{3,30}", ErrorMessage = "Not valid name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Middle name")]
+        [RegularExpression("[a-zA-Z]{3,30}", ErrorMessage = "Not valid name")]
+        public string MiddleName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        [DisplayName("Passport")]
+        [RegularExpression("([A-Z]{2}[0-9]{7})", ErrorMessage = "Enter only UPPERCASE alphabets and numbers")]
+        public string Passport { get; set; }
+
+        [DisplayName("Identification No.")]
+        [RegularExpression("([A-Z0-9]{14})", ErrorMessage = "Enter only UPPERCASE alphabets and numbers")]
+        public string Identification { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
