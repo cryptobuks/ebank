934ffa2415a22ff9d811f1b687640299834533d6
pkartsel <pkartsel@gmail.com>
2013-12-19 03:19:12 +0300

    jquery-ui, visual changes

--- Proebank/PresentationLayer/Presentation/App_Start/BundleConfig.cs
+++ Proebank/PresentationLayer/Presentation/App_Start/BundleConfig.cs
@@ -10,6 +10,9 @@ namespace Presentation
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-1.10.3.custom.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -22,6 +25,9 @@ namespace Presentation
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new StyleBundle("~/bundles/jquery-ui").Include(
+                      "~/Content/jquery-ui-1.10.3.custom.min.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
@@ -31,6 +37,9 @@ namespace Presentation
 
             bundles.Add(new ScriptBundle("~/bundles/Chart").Include(
             "~/Scripts/Chart.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/proebank").Include(
+            "~/Scripts/proebank.js"));
         }
     }
 }
