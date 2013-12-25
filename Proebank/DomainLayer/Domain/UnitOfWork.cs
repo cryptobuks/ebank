0437571326c32e849d3a1e1dbb4057213100e00f
Dmitry Linov <dimorph@live.ru>
2013-12-25 22:15:07 +0300

    Added file

---
+++ Proebank/DomainLayer/Domain/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _ctx;
+
+        public IdentityDbContext<IdentityUser> Context { get { return _ctx; } }
+
+        public UnitOfWork(DataContext context)
+        {
+            _ctx = context;
+        }
+
+        public void SaveChanges()
+        {
+            _ctx.SaveChanges();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _ctx.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Context.Dispose();
+            }
+        }
+
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
+    }
+}
